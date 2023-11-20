cc46860760d32c6dda09bb57db2494602d7d26d8
PJPhukan <paragjyotiphukan2004@gmail.com>
2023-11-21 00:21:08 +0530

    Cart section completed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,45 +7,63 @@ import { CgShoppingCart } from "react-icons/cg"
 import { AiOutlineHeart } from "react-icons/ai"
 
 // import Search from './Search/Search'
-// import Cart from '../Cart/Cart'
+import Cart from '../Cart/Cart'
 // import Context from '../../utails/context'
 import "./Header.scss";
 const Header = () => {
-const [scrolled, setScrolled] = useState(false)
-    const handleScroll=()=>{
-        const offset=window.scrollY
-        if (offset>150) {
+
+    //USESTATE
+    //usestate for scrolled navber
+    const [scrolled, setScrolled] = useState(false)
+    //Show cart usestate
+    const [showCart, setShowCart] = useState(false)
+
+
+    //handle navber scroll function
+    const handleScroll = () => {
+        const offset = window.scrollY
+        if (offset > 150) {
             setScrolled(true)
         }
-        else{
+        else {
             setScrolled(false)
         }
     }
     useEffect(() => {
-      window.addEventListener("scroll",handleScroll)
+        window.addEventListener("scroll", handleScroll)
     }, [])
-    
+
+    //functions for show cart
+    const showCartFunction = () => {
+        setShowCart(true);
+
+    }
+
     return (
-        <header className={`main-header ${scrolled?"sticky-header":""}`}>
-            <div className="header-content">
-                <ul className="left">
-                    <li>home</li>
-                    <li>about</li>
-                    <li>categories</li>
-                </ul>
-                <div className="center">
-                    PJSTORE.
+        <>
+            <header className={`main-header ${scrolled ? "sticky-header" : ""}`}>
+                <div className="header-content">
+                    <ul className="left">
+                        <li>home</li>
+                        <li>about</li>
+                        <li>categories</li>
+                    </ul>
+                    <div className="center">
+                        PJSTORE.
+                    </div>
+                    <div className="right">
+                        <TbSearch />
+                        <AiOutlineHeart />
+                        <span className="card-icon" onClick={showCartFunction}>
+                            <CgShoppingCart />
+                            <span>5</span>
+                        </span>
+                    </div>
                 </div>
-                <div className="right">
-                    <TbSearch />
-                    <AiOutlineHeart />
-                    <span className="card-icon">
-                        <CgShoppingCart />
-                        <span>5</span>
-                    </span>
-                </div>
-            </div>
-        </header>
+            </header>
+            {showCart && <Cart setShowCart={setShowCart}/>}
+        </>
+
     );
 };
 
